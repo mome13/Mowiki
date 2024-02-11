ba1ebed8d67053a2efed180450e679e55bdee532
mehran13mome <mehran13mome@gmail.com>
2024-02-11 14:19:43 +0330

    Update main-nav
      -  remove special picture in first menu dropdown

--- components/layouts/main-nav.tsx
+++ components/layouts/main-nav.tsx
@@ -37,79 +37,40 @@ export default function Navigation({ items }: NavigationProps) {
       </Link>
       <NavigationMenu>
         <NavigationMenuList>
-          {items?.[0]?.items ? (
-            <NavigationMenuItem>
-              <NavigationMenuTrigger className='h-auto'>
-                {items[0].title}
-              </NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <ul className='grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]'>
-                  <li className='row-span-3'>
-                    <NavigationMenuLink asChild>
-                      <a
-                        aria-label='Home'
-                        className='flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md'
-                        href='/'
+          {items?.map((item) =>
+            item?.items ? (
+              <NavigationMenuItem key={item.title}>
+                <NavigationMenuTrigger className='h-auto capitalize'>
+                  {item.title}
+                </NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <ul className='grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]'>
+                    {item.items.map((item) => (
+                      <ListItem
+                        key={item.title}
+                        title={item.title}
+                        href={item.href}
                       >
-                        <Icons.logo className='h-6 w-6' aria-hidden='true' />
-                        <div className='mb-2 mt-4 text-lg font-medium'>
-                          {siteConfig.name}
-                        </div>
-                        <p className='text-sm leading-tight text-muted-foreground'>
-                          {siteConfig.description}
-                        </p>
-                      </a>
-                    </NavigationMenuLink>
-                  </li>
-                  {items[0].items.map((item) => (
-                    <ListItem
-                      key={item.title}
-                      title={item.title}
-                      href={item.href}
-                    >
-                      {item.description}
-                    </ListItem>
-                  ))}
-                </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-          ) : null}
-          {items
-            ?.filter((item) => item.title !== items[0]?.title)
-            .map((item) =>
-              item?.items ? (
+                        {item.description}
+                      </ListItem>
+                    ))}
+                  </ul>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+            ) : (
+              item.href && (
                 <NavigationMenuItem key={item.title}>
-                  <NavigationMenuTrigger className='h-auto capitalize'>
-                    {item.title}
-                  </NavigationMenuTrigger>
-                  <NavigationMenuContent>
-                    <ul className='grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]'>
-                      {item.items.map((item) => (
-                        <ListItem
-                          key={item.title}
-                          title={item.title}
-                          href={item.href}
-                        >
-                          {item.description}
-                        </ListItem>
-                      ))}
-                    </ul>
-                  </NavigationMenuContent>
+                  <Link href={item.href} legacyBehavior passHref>
+                    <NavigationMenuLink
+                      className={cn(navigationMenuTriggerStyle(), 'h-auto')}
+                    >
+                      {item.title}
+                    </NavigationMenuLink>
+                  </Link>
                 </NavigationMenuItem>
-              ) : (
-                item.href && (
-                  <NavigationMenuItem key={item.title}>
-                    <Link href={item.href} legacyBehavior passHref>
-                      <NavigationMenuLink
-                        className={cn(navigationMenuTriggerStyle(), 'h-auto')}
-                      >
-                        {item.title}
-                      </NavigationMenuLink>
-                    </Link>
-                  </NavigationMenuItem>
-                )
               )
-            )}
+            )
+          )}
         </NavigationMenuList>
       </NavigationMenu>
     </div>
